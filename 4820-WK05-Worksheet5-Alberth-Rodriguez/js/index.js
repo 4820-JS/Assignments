5615f65ed0e035d8411eb977e6c7197a0b2073af
ALFAROSO <87240930+ALFAROSO@users.noreply.github.com>
2023-06-08 22:28:36 -0700

    Add files via upload

---
+++ 4820-WK05-Worksheet5-Alberth-Rodriguez/js/index.js
@@ -0,0 +1,25 @@
+//This is a dance move application that allows for them to
+// be saved in a list form
+
+// You will need an array to save the entries to, and an event listener,
+// like we went over in class, to save the entries when a button is clicked
+
+// *** Hint: Look at the HTML IDs on how to select elements.
+let danceArr = []
+danceList.innerHTML = "<h2>Your Dance Moves:</h2>"
+
+
+submitBtn.addEventListener("click", function(){
+    if(danceInput.value.length > 0){
+    let resut = ""
+    let inputVal = danceInput.value
+    //
+    danceArr.push(inputVal)
+
+    for(let i = 0; i < danceArr.length; i++){
+        resut += `• Dance move #${[i+1]}: ${danceArr[i]}<br>`
+    }
+    // danceList.innerHTML = "<h2>Your Dance Moves:</h2>"
+    danceList.innerHTML = "<h2>Your Dance Moves:</h2>"+resut
+    }
+})
