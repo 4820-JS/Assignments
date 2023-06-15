068226b9403420061eacaec64131304ce7cd8a66
ALFAROSO <87240930+ALFAROSO@users.noreply.github.com>
2023-06-14 22:59:34 -0700

    Add files via upload

---
+++ 4820-WK06-Worksheet4-Alberth-Rodriguez/index.js
@@ -0,0 +1,49 @@
+console.log ("test")
+//======================================================================
+//(1). In your own words, explain the difference between a selection and an event listener.
+
+//======================================================================
+
+//(2). What are two ways to round up a decimal number to an integer?
+
+//======================================================================
+
+//(3). What does .toFixed() do, what do you put in the brackets, and does it round up or down?
+
+//======================================================================
+
+//(4). What datatype do we get from an input box when we use .value?
+
+//======================================================================
+
+//(5). Using the HTML code below, create a currency converter, to change Canadian dollars to British pounds or vice versa, depending on which button is clicked . Output to the UI (webpage) a message that says the original amount, and the changed amount. 
+
+
+//======================================================================
+
+//(6). Using the HTML below, write code that allows the user to add flower types to a bouquet, and output a list to the UI. The flower types cannot repeat.
+
+
+//======================================================================
+
+//(7). Using the HTML code below, write code that creates a bill splitter for a restaurant. The UI should have an output that breaks down all the information given. Therefore, it needs to include the original bill, the tax amount, the tip amount, the number of people to split the bill between, the total bill with tax and tip added, and finally, the split amount. For example:
+
+// Original bill amount: $50.00
+// Tax amount: $5.00
+// Tip amount: $ 10.00
+// Split between: 5 people
+// Total bill: $65.00
+// Split total: $13.00
+
+
+//======================================================================
+
+//(8). Using the HTML code below, write some code that converts temperature from Celsius to Fahrenheit, or from Fahrenheit to Celsius, depending on the button pressed. You will have to search the conversion formula. 
+
+
+
+//======================================================================
+
+
+
+//======================================================================
