d65bca5978de3fff129f1531d5150ec15b63f335
ALFAROSO <87240930+ALFAROSO@users.noreply.github.com>
2023-07-02 17:41:58 -0700

    Update index.js

--- 4820-WK08-Worksheet6-Alberth-Rodriguez/index.js
+++ 4820-WK08-Worksheet6-Alberth-Rodriguez/index.js
@@ -43,4 +43,46 @@ console.log("test")
 // that could be associated theo the real world.
 // in short word arrays are for simple data and objects for more complex data.
 
+//======================================================================
+
+//(5) There is a common thought that a dog’s age is seven times that of a human age.
+console.log('5 ========================================================================')
+//............Setup:........................
+// Create an array of objects, called dogArr, which has 3 objects inside it.
+// Each object will have three parameters: name, breed, and age
+
+//............Problem:........................
+// Create a function called dogsAge that takes in an array of objects as a parameter,
+// that sums the dogs’ ages, and converts that sum from human years to dog years. 
+// The function returns a string that says: 
+// “<name1>, <name2>, and <name3>’s combined ages is <converted age> in dog years”
+
+//............Allowed tools:........................
+// Loops, if/else, switch, variables, arrays (no array methods),
+// functions (you can use arrow functions),
+// Math library (Math.floor, Math.ceil, Math.round, Math.random),
+// toFixed(), calculations and booleans (+,-,/,*,%, <, <=, >=, >, ===)
+
+
+let dogArr = [{name:"Cooper", breed:"Boxer",   age:1},
+              {name:"Duke",   breed:"PitBull", age:2},
+              {name:"Coco",   breed:"Rolo",    age:3}
+]//Define Array of objects whit parameters 
+
+function ages(dogObj){//define the function
+    let dog = "" //define varible to save every object temporary
+    let age = 0 //define varible to save ages 
+    let names = [] //define an empty array
+    for (let i = 0; i < dogObj.length; i++) {
+        dog = dogObj[i]
+        age = age + dog.age //saving and adding age
+        names.push(dog.name) //saving names
+    }
+    answ = `"${names[0]}, ${names[1]}, ${names[2]}'s combined ages is ${age*7} in dog years"` //saving the answer
+    return answ
+}
+
+console.log(ages(dogArr))
+
+
 
