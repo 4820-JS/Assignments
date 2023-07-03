238099f4441848155996bb6470fd23bfdae5ce5b
ALFAROSO <87240930+ALFAROSO@users.noreply.github.com>
2023-07-02 17:39:48 -0700

    Update index.js

--- 4820-WK08-Worksheet6-Alberth-Rodriguez/index.js
+++ 4820-WK08-Worksheet6-Alberth-Rodriguez/index.js
@@ -1,1 +1,13 @@
 console.log("test")
+
+//(1) In your own words, explain two ways that objects are different from arrays.
+
+//Usage:
+// With objects you can model real world you can create an idea and manipulate its attributes.
+// Arrays are mainly to save elements.
+
+//Structure: 
+// Objects are collections of keys and values organized in pairs like "Key: Value"
+// Arrays are collections of elements most of the times of the same data type (string, number, boolean...) but also combined 
+
+//======================================================================
