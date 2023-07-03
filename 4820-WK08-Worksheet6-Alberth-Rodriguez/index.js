182e0b913fd67816a1353c1ffe94181552847567
ALFAROSO <87240930+ALFAROSO@users.noreply.github.com>
2023-07-02 17:42:44 -0700

    Update index.js

--- 4820-WK08-Worksheet6-Alberth-Rodriguez/index.js
+++ 4820-WK08-Worksheet6-Alberth-Rodriguez/index.js
@@ -84,5 +84,70 @@ function ages(dogObj){//define the function
 
 console.log(ages(dogArr))
 
+//======================================================================
+console.log('6 ========================================================================')
+
+//(6).............Problem:
+// Create an object for a video rental store. It should have the following parameters
+// with values added to make key/value pairs:
+// movieName, 
+// numberOfCopies,
+// numberRented,
+// genre,
+// isForKids,
+// alternateRentalTitles (at least 3).
+// The object should have the following methods: rent, return, checkIfAvailable.
+// Fill in the methods with code, using the keyword ‘this’ as we learned in class.
+// If the movie is not available, there will be a message to the console that offers one 
+// of the alternative movies to rent, at random.
+
+//.............Allowed tools:
+// Loops, if/else, switch, variables, arrays (no array methods),
+// functions (you can use arrow functions),
+// Math library (Math.floor, Math.ceil, Math.round, Math.random),
+// toFixed(), calculations and booleans (+,-,/,*,%, <, <=, >=, >, ===)
+
+//define the object
+let movieStore = { // define parameters
+    movieName:"The Black Demon",
+    numberOfCopies:3,
+    numberRented:1,
+    genre:"horror",
+    isForKids:false,
+    alternateRentalTitles:["Evil Dead Rise","Smile","Malum"],
+
+    rent: function(){ // method to rent a movie
+        this.numberRented++
+    },
+    return: function(){ //method to return a copy
+        this.numberRented--
+    },
+    checkIfAvailable: function(){ //method to check availability
+        let availableCopies = this.numberOfCopies - this.numberRented //finding out if therre is an availabel copy
+        let randoMovie = this.alternateRentalTitles[Math.floor(Math.random() * this.alternateRentalTitles.length)] //other option
+        let answ = ''
+        // console.log(availableCopies) // this line of code is to test the availabel copies
+        if(availableCopies === 1){
+            answ = console.log(`There is ${availableCopies} left`) //print out the amount of copies availabel for a singular answer (is)
+        }else if (availableCopies > 1){
+            answ = console.log(`There are ${availableCopies} left`) //print out the amount of copies availabel for a plural answer (are)
+        }
+        else {
+            answ = console.log(`There are no copies of this movie you can rent "${randoMovie}".`) //printing out other option if there are no copies available
+        }
+        return answ //return the answer
+    }
+}
+
+movieStore.rent()//rent 1 so 1 available
+movieStore.return() //returnt 1 so 2 available 
+movieStore.return() //returnt 1 so 3 available
+movieStore.rent() //rent 1 so 2 available
+movieStore.rent() //rent 1 so 1 available
+movieStore.rent() //rent 1 so 0 available
+movieStore.checkIfAvailable()
+
+
+
 
 
