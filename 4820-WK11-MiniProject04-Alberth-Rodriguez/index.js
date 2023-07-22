354e01a7f6cc418334e9469f0bb53d01535b8b6c
ALFAROSO <87240930+ALFAROSO@users.noreply.github.com>
2023-07-21 19:24:43 -0700

    Add files via upload

---
+++ 4820-WK11-MiniProject04-Alberth-Rodriguez/index.js
@@ -0,0 +1,14 @@
+// Create a recipe box app, where the user can search by first letter or by meal name. 
+// The data will come from an api call from the website https://www.themealdb.com/api.php
+// Please visit the website and try different searches beforehand. 
+
+// When the user presses the search button, the data from the api is put into a table. Please refer
+// to the screenshots. 
+
+// If the checkboxes are selected, and the user presses the "more info" button, then the cooking
+// instructions appear below the table, and the button disappears. 
+
+// As always, you must include comments to recieve marks on this assignment. No comments = no marks.
+// You are NOT allowed to modify the CSS or HTML file.
+// You are NOT allowed to use concepts that we have not covered in class. If you need clarification,
+// please message me. 
